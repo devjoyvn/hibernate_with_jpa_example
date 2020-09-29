fbd344cf81d6456d0fc1694e3c56d463a2ed772a
hai.tn <hai.nguyenthanh@cyberlogitec.com>
2020-09-29 13:38:03 +0700

    update config example

---
+++ config-example/src/main/java/PersistExample.java
@@ -0,0 +1,22 @@
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import entities.*;
+
+public class PersistExample {
+
+    public static void main(String[] agrs) {
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("helloword");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Message message = new Message();
+        message.setText("Hello World!");
+        em.persist(message);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+    }
+}
